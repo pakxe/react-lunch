5a06e1a28326a8777ef3627a2afa7d2839c3c9d8
bmj0053 <bmj05378@gmail.com>
2025-01-04 01:12:05 +0900

    chore: typescript 및 eslint 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import "./App.css";
-
 function App() {
   return <h1>Self-Paced React</h1>;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './App.tsx';
 
 async function enableMocking() {
-  const { worker } = await import('./mocks/browser');
+  const { worker } = await import('./mocks/browser.js');
 
   return worker.start();
 }
 
 enableMocking().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')).render(
+  ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
