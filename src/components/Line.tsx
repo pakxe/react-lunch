c2e39d953d3b6a247a60d8d0933a28731a043d5c
pakxe <pigkill40@naver.com>
2025-01-11 00:37:09 +0900

    feat: color, height를 받아 렌더링하도록 Line 컴포넌트 수정

--- src/components/Line.tsx
+++ src/components/Line.tsx
@@ -1,12 +1,18 @@
 import { css, useTheme } from '@emotion/react';
+import theme from '../theme';
 
-const Line = () => {
+type LineProps = {
+  color?: keyof typeof theme.colors;
+  height?: number;
+};
+
+const Line = ({ color, height }: LineProps) => {
   const theme = useTheme();
 
   return (
     <hr
       css={css`
-        border: 1px solid ${theme.colors.gray2};
+        border-bottom: ${height ?? 1}px solid ${color ? theme.colors[color] : theme.colors.gray2};
         width: 100%;
       `}
     />
