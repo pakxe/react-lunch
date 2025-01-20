06fd378daecbac603374c16108a46a978bcaf996
pakxe <pigkill40@naver.com>
2025-01-20 14:16:31 +0900

    feat: 카테고리 value, name 을 가진 배열 상수 선언

--- src/constants/category.ts
+++ src/constants/category.ts
@@ -1,1 +1,12 @@
+import { Category } from '../types/serviceType';
+
 export const CATEGORY = ['korean', 'chinese', 'japanese', 'western', 'asian', 'etc'] as const;
+
+export const CATEGORY_LIST: { value: Category; name: string }[] = [
+  { value: 'korean', name: '한식' },
+  { value: 'chinese', name: '중식' },
+  { value: 'japanese', name: '일식' },
+  { value: 'western', name: '양식' },
+  { value: 'asian', name: '아시안' },
+  { value: 'etc', name: '기타' },
+];
