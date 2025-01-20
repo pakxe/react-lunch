fcf58ea8f27f54a1f0e501685d1622947edc6280
pakxe <pigkill40@naver.com>
2025-01-20 14:16:38 +0900

    feat: 이동 시간 value, name 을 가진 배열 상수 선언

---
+++ src/constants/timeToMove.ts
@@ -0,0 +1,8 @@
+import { TimeToMove } from '../types/serviceType';
+
+export const TIME_TO_MOVE_LIST: { value: TimeToMove; name: string }[] = [
+  { value: 5, name: '5분' },
+  { value: 10, name: '10분' },
+  { value: 15, name: '15분' },
+  { value: 20, name: '20분' },
+];
